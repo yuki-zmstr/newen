bd48363ddf0d62577410ce18111268e889d717d6
Yukihide Takahashi <yukihide99@gmail.com>
2022-04-11 10:59:14 +0900

    more stuff

--- js/script.js
+++ js/script.js
@@ -1,18 +1,3 @@
 $(document).ready(function() {
-  var wh = $(window).height();
 
-  // project description pop up
-  $popup = $('.popup');
-  $(document).mousemove(function(){
-    $popup.each(function() {
-      var $this = $(this)
-      var to_pop = "#" + $this.data('name');
-      var $to_pop = $(to_pop)
-      $this.hover(function(){
-        $to_pop.fadeIn(300);
-      }, function() {
-        $to_pop.fadeOut(300)
-      })
-    })
-  })
 });
