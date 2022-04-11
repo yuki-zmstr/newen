89f7125d7aa5fffc5621055bf5fea55cff8086a2
Yukihide Takahashi <yukihide99@gmail.com>
2022-04-11 15:59:27 +0900

    scripts

--- js/script.js
+++ js/script.js
@@ -1,3 +1,33 @@
-$(document).ready(function() {
+$(document).ready( function(){ 
+  
+  let slideIndex = 1;
+  showSlides(slideIndex);
 
-});
+  $(".prev").click(function() {
+    showSlides(slideIndex -= 1)
+  })
+  $(".next").click(function() {
+    showSlides(slideIndex += 1)
+  })
+
+  $('.dot').click(function(){
+    let num = $(this).data('num')
+    showSlides(slideIndex = num)
+  })
+
+  function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+  }
+})
