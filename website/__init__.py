0173cd8d637a293c777ea2e0532ba8dbacb7188c
Yukihide Takahashi <yukihide99@gmail.com>
2022-11-27 15:16:55 +0900

    changes for eb

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from os import path
 
+
 def create_app():
-    app = Flask(__name__)
+    application = Flask(__name__)
     from .views import views
-    app.register_blueprint(views, url_prefix='/')
-    return app
+    application.register_blueprint(views, url_prefix='/')
+    return application
