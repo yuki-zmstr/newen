67007f616c92cac59ee2e77f5704bd1de4fff481
Yukihide Takahashi <yukihide99@gmail.com>
2022-12-02 20:59:07 +0900

    removed flask related stuff

--- website/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from os import path
-
-
-def create_app():
-    application = Flask(__name__)
-    from .views import views
-    application.register_blueprint(views, url_prefix='/')
-    return application
